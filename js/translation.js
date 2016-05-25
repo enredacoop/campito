4dfdbb7c3a3cf967101ff15ed95d0353555e00a1
pr3ssh <pablo.martin@enreda.coop>
2016-05-25 11:43:55 +0200

    Updating i18n code

--- js/translation.js
+++ js/translation.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
   
   i18n.init({ preload: ['en', 'es'], debug: true, detectLngQS: 't', fallbackLng:'es' }, function() {
-      // save to use translation function as resources are fetched
-     $('[data-i18n]').i18n();          
+    // save to use translation function as resources are fetched
+    $('[data-i18n]').i18n();          
   });
 
-	 $('.lang').click(function () {
-        var lang = $(this).attr('data-lang');
-        i18n.setLng(lang, function() { $('[data-i18n]').i18n(); });
-    });
+  $('#changelang').click(function () {
+    var lang = $(this).attr('data-lang');
+    var name = '';
+    i18n.setLng(lang, function() { $('[data-i18n]').i18n(); });
+    if (lang == 'es') {
+      lang = 'en';
+      name = 'English';
+    } else {
+      lang = 'es';
+      name = 'Spanish';
+    }
+    $(this).attr('data-lang', lang);
+    $(this).html(name);
+  });
 
 });
